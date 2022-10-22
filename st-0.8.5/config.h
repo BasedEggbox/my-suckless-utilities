b1db67b565a6f2df4c368154d9e98ddf90bb7876
BasedEggbox <dr.roboeggnik@proton.me>
2022-10-22 15:38:06 +0100

    I just changed the st font color, no major changes.

--- st-0.8.5/config.h
+++ st-0.8.5/config.h
@@ -96,24 +96,24 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-  [0] = "#0f1118", /* black   */
-  [1] = "#181a24", /* red     */
-  [2] = "#282c39", /* green   */
-  [3] = "#3e4357", /* yellow  */
-  [4] = "#5b617c", /* blue    */
-  [5] = "#7f86a4", /* magenta */
-  [6] = "#adb1c5", /* cyan    */
-  [7] = "#e1e2e9", /* white   */
+  [0] = "#282a2e", /* black   */
+  [1] = "#a54242", /* red     */
+  [2] = "#8c9440", /* green   */
+  [3] = "#de935f", /* yellow  */
+  [4] = "#5f819d", /* blue    */
+  [5] = "#85678f", /* magenta */
+  [6] = "#5e8d87", /* cyan    */
+  [7] = "#707880", /* white   */
 
    /* 8 bright colors */
-  [8]  = "#7ed69a", /* black   */
-  [9]  = "#ebaca5", /* red     */
-  [10] = "#bab281", /* green   */
-  [11] = "#6cd38e", /* yellow  */
-  [12] = "#e26dce", /* blue    */
-  [13] = "#b8ad60", /* magenta */
-  [14] = "#b5bddf", /* cyan    */
-  [15] = "#d668c3", /* white   */
+  [8]  = "#373b41", /* black   */
+  [9]  = "#cc6666", /* red     */
+  [10] = "#b5bd68", /* green   */
+  [11] = "#f0c674", /* yellow  */
+  [12] = "#81a2be", /* blue    */
+  [13] = "#b294bb", /* magenta */
+  [14] = "#8abeb7", /* cyan    */
+  [15] = "#c5c8c6", /* white   */
 
 	[255] = 0,
 
